c8b7b2992a898724a1e50fff08985149e82e4abc
steedn <steedn@outlook.com>
2021-06-07 09:13:59 -0600

    second commit for feedback

--- obj1.py
+++ obj1.py
@@ -12,7 +12,7 @@ class Percept:
         self.weights = [random.random(), random.random(), random.random(), random.random()
         , random.random(), random.random(), random.random(), random.random()
         , random.random(), random.random()]
-        # self.outputP = 0
+        
     def getData(self):
         df = pd.read_csv('./housepricedata.csv')
         dataset = df.values
@@ -22,6 +22,7 @@ class Percept:
         self.x_scale = min_max_scaler.fit_transform(x)
         self.max = min_max_scaler.data_max_
         self.min = min_max_scaler.data_min_
+
     def calc(self, inputs, output):
         outputP = inputs[0]*self.weights[0] + inputs[1]*self.weights[1] + inputs[2]*self.weights[2]
         + inputs[3]*self.weights[3] + inputs[4]*self.weights[4] + inputs[5]*self.weights[5] + inputs[6]*self.weights[6]
@@ -31,15 +32,11 @@ class Percept:
         error = output - outputP
         self.bweight += error * self.bias*self.lr
         for i in range(10):
-            # print(i)
             self.weights[i] += error * inputs[i]* self.lr
 
     def learn(self):
         for i in range(51):
-            if i % 50 == 0:
-                print(i)
             for i in range(1460):
-                # print(i)
                 self.calc(self.x_scale[i], self.y[i])
 
     def guess(self, input):
